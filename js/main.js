3ef6e9f51f679321351c5883502d609ef884ff47
James OBENG <20jobeng@chelsea-academy.org>
2026-03-12 11:04:11 +0000

    Add input validation and previous guess tracking

--- js/main.js
+++ js/main.js
@@ -1,9 +1,23 @@
 let target = Math.floor(Math.random() * 10) + 1;
 let attempts = 0;
+let prevGuess = null;
 
 const message = document.getElementById('message');
 const attemptDisplay = document.getElementById('attempt-count');
 const resetbtn = document.getElementById('reset-btn');
+const input = document.getElementById('guess');
+const submitbtn = document.getElementById("submit-btn");
+
+input.addEventListener('input', function() {
+    this.value = this.value.replace(/[^0-9]/g, '');
+
+    if (parseInt(this.value) > 10) {
+        this.value = 10;
+    }
+
+    submitbtn.disabled = !this.checkValidity();
+});
+
 
 document.getElementById('game-form').onsubmit = function(event) {
 
@@ -12,9 +26,16 @@ document.getElementById('game-form').onsubmit = function(event) {
     }
 
     event.preventDefault();
-    const guess = Number(document.getElementById('guess').value);
-    
-    attempts++;
+    const guess = Number(input.value);
+
+    if (prevGuess === guess) {
+        message.textContent = "You just guessed that, try again!";
+        message.style.color = "red";
+        return;
+    }
+
+    attempts++
+
     attemptDisplay.textContent = attempts;
 
     if (guess === target) {
@@ -24,9 +45,20 @@ document.getElementById('game-form').onsubmit = function(event) {
     } else if (guess > target) {
         message.textContent = "Too high! Try lower.";
         message.style.color = "red";
+        prevGuess = guess;
     } else {
         message.textContent = "Too low! Try higher.";
         message.style.color = "red";
+        prevGuess = guess;
+
+    }
+
+    if (attempts >= 10) {
+        attemptDisplay.style.color = "red";
+    } else if (attempts > 5) {
+        attemptDisplay.style.color = "#FFBF00";
+    } else {
+        attemptDisplay.style.color = "green";
     }
 };
 
@@ -43,6 +75,7 @@ resetbtn.onclick = function() {
     attemptDisplay.textContent = "0";
     message.textContent = "";
     document.getElementById('guess').value = "";
+    prevGuess = 0;
 
     resetbtn.style.display = "none";
     document.getElementById("submit-btn").style.display = "block";
